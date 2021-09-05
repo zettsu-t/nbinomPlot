b5cf1479ad16e8d0e80824584e1eb4b97e20feee
zettsu-t <zettsu.tatsuya@gmail.com>
2021-09-05 06:26:54 +0100

    Download automatically

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -144,8 +144,14 @@ test_that("Download", {
   app$setValue(NS("nbplot", "fix"), "size")
   app$waitForShiny()
 
-  set_and_download <- function(size, prob, quantile, expected) {
+  compare_downloaded_file <- function(df_actual, expected) {
+    tolerance <- 1e-7
+    expect_equal(object = NROW(df_actual), NROW(expected))
+    expect_equal(object = df_actual$x, expected = 0:(NROW(expected) - 1))
+    expect_equal(object = df_actual$density, expected = expected, tolerance = tolerance)
+  }
 
+  set_and_download <- function(size, prob, quantile, expected) {
     app$setValue(NS("nbplot", "quantile"), quantile)
     app$waitForShiny()
     app$setValue(NS("nbplot", "size"), size)
@@ -167,18 +173,30 @@ test_that("Download", {
     actual_filename <- file.path(download_dir, download_filename)
     df_actual <- readr::read_csv(actual_filename)
 
-    tolerance <- 1e-7
-    expect_equal(object = NROW(df_actual), NROW(expected))
-    expect_equal(object = df_actual$x, expected = 0:(NROW(expected) - 1))
-    expect_equal(object = df_actual$density, expected = expected, tolerance = tolerance)
+    compare_downloaded_file(df_actual = df_actual, expected = expected)
+  }
+
+  download_without_dialog <- function(app, id) {
+    xpath <- paste0("//*[@id='", id, "']")
+    elements <- app$findElements(xpath = xpath)
+    df <- NULL
+    if (NROW(elements) == 1) {
+      filename <- tempfile()
+      curl::curl_download(elements[[1]]$getAttribute("href"), filename)
+      df <- readr::read_csv(filename)
+    }
+    df
   }
 
   size_first <- 4
   prob_first <- 0.75
   quantile_first <- 0.99
   expected_first <- c(0.31640625, 0.31640625, 0.19775391, 0.09887695, 0.04325867, 0.01730347)
+
   set_and_download(size = size_first, prob = prob_first, quantile = quantile_first,
                    expected = expected_first)
+  df_actual <- download_without_dialog(app = app, id = NS("nbplot", "download"))
+  compare_downloaded_file(df_actual = df_actual, expected = expected_first)
 
   size_second <- 8
   prob_second <- 0.85
@@ -187,6 +205,8 @@ test_that("Download", {
                        0.045522948, 0.016388261, 0.005326185, 0.001597855)
   set_and_download(size = size_second, prob = prob_second, quantile = quantile_second,
                    expected = expected_second)
+  df_actual <- download_without_dialog(app = app, id = NS("nbplot", "download"))
+  compare_downloaded_file(df_actual = df_actual, expected = expected_second)
 
   app$stop()
 })
