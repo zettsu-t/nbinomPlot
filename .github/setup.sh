2ab07dab74629314adebe598ceded41cb748d8d8
zettsu-t <zettsu.tatsuya@gmail.com>
2021-12-17 10:09:10 +0000

    Enable caching

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#sudo apt-get install -y git-core imagemagick libboost-dev libcairo2-dev libcurl4-openssl-dev libgit2-dev libicu-dev libmagic-dev libmagick++-dev libpng-dev libssl-dev libxml2-dev make pandoc pandoc-citeproc qpdf zlib1g-dev
+sudo apt-get install -y git-core imagemagick libboost-dev libcairo2-dev libcurl4-openssl-dev libgit2-dev libicu-dev libmagic-dev libmagick++-dev libpng-dev libssl-dev libxml2-dev make pandoc pandoc-citeproc qpdf zlib1g-dev
 Rscript -e 'install.packages(c("remotes"))'
-#Rscript -e 'remotes::install_deps(dependencies = TRUE)'
+Rscript -e 'remotes::install_deps(dependencies = TRUE)'
